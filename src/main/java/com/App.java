f2a21773bb34a7d4e3eb211b94251c251703dd8c
Tiago <tiagolachman@gmail.com>
2023-06-18 21:13:00 -0300

    Update

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -8,49 +8,56 @@ import com.model.*;
 public class App {
     public static void main(String[] args) {
         Cliente cliente = new Cliente();
+        Produto produto = new Produto();
         Produto produto1 = new Produto();
-        Produto produto2 = new Produto("mouse", 50, 60.0, "eletronico");
-        Produto produto3 = new Produto("notebook", 20, 1500.0, "eletronico");
-        Produto produto4 = new Produto("teclado", 60, 101.0, "eletronico");
+        Produto produto2 = new Produto();
+        Produto produto3 = new Produto();
+        Produto produto4 = new Produto();
         Pedido pedido = new Pedido();
+        
+
+        int op = 0;
         try {
 
-            //cliente = new Cliente("Tiago", "André", 22, "123456789", "Rua", "8000000", "@gmail.com", "419999999", "Rua");
+            cliente = new Cliente("Tiago", "André", 22, "123456789", "Rua", "8000000", "@gmail.com", "419999999",
+                    "Rua");
+            System.out.println("\n\nOi");
             //ClienteController.cadastrarCliente(cliente);
             cliente = ClienteController.buscarClientePorCPF("123456789");
 
-            //produto1 = new Produto("tv", 100, 30.0, "eletronico");
+            produto1 = new Produto("tv", 100, 30.0, "eletronico");
+            produto2 = new Produto("mouse", 100, 30.0, "eletronico");
+            produto3 = new Produto("notebook", 100, 30.0, "eletronico");
+            produto4 = new Produto("teclado", 100, 30.0, "eletronico");
 
+            
+            
             //ProdutoController.cadastrarProduto(produto1);
-
+            ProdutoController.cadastrarProduto(produto2);
+            ProdutoController.cadastrarProduto(produto3);
+            ProdutoController.cadastrarProduto(produto4);
+            
             produto1 = ProdutoController.buscarProdutoPorNome("tv");
-
-            // ProdutoController.cadastrarProduto(produto2);
-            // ProdutoController.cadastrarProduto(produto3);
-            // ProdutoController.cadastrarProduto(produto4);
-
             produto2 = ProdutoController.buscarProdutoPorNome("mouse");
             produto3 = ProdutoController.buscarProdutoPorNome("notebook");
             produto4 = ProdutoController.buscarProdutoPorNome("teclado");
 
-            // pedido = new Pedido(LocalDateTime.now());
-            // pedido.setCliente(cliente);
+            pedido = new Pedido(LocalDateTime.now());
+            pedido.setCliente(cliente);
 
-            // PedidoController.adicionarNoCarrinho(pedido, produto1, 1);
-            // PedidoController.adicionarNoCarrinho(pedido, produto2, 1);
-            // PedidoController.adicionarNoCarrinho(pedido, produto3, 1);
-            // PedidoController.adicionarNoCarrinho(pedido, produto4, 1);
+            PedidoController.adicionarNoCarrinho(pedido, produto1, 1);
+            PedidoController.adicionarNoCarrinho(pedido, produto2, 1);
+            PedidoController.adicionarNoCarrinho(pedido, produto3, 1);
+            PedidoController.adicionarNoCarrinho(pedido, produto4, 1);
 
-            // PedidoController.cadastrarPedido(pedido);
-
-            
+            PedidoController.cadastrarPedido(pedido);
 
             pedido = PedidoController.buscarPedidoPorId(2);
-            
-            // PedidoController.alterarCarrinho(pedido, produto4, 10);
 
-            System.out.println("\n\nQuantidade\n\n"+pedido.getProdutos().get(3).getQuantidadeEstoque());
-            //System.out.println(pedido.getProdutos());
+            PedidoController.alterarCarrinho(pedido, produto4, 10);
+
+            System.out.println("\n\nQuantidade\n\n" + pedido.getProdutos().get(3).getQuantidadeEstoque());
+            // System.out.println(pedido.getProdutos());
             PedidoController.alterarPedido(pedido.getId(), pedido);
 
             pedido = PedidoController.buscarPedidoPorId(2);
